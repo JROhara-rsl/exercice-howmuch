3510da88107acf206643f0049789a1bf7513513f
Julien Roussel <roussel-julien@outlook.fr>
2025-01-02 15:06:08 +0100

    Maj générale

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const form = document.getElementById('form');
 const input = document.getElementById('guessInput');
 const feedback = document.getElementById('feedback');
+const cartouche = document.getElementById('cartouche');
 
 // 2 - Générer un nombre aléatoire
 // Arrow function qui donne un chiffre entre min et max
@@ -10,13 +11,24 @@ var goodNb = nbRandom(1, 100);
 console.log(goodNb);
 var essai = 0;
 
+// 3 - Créer des blocs de cartouche pour répresenter le nombre d'essaie qu'il reste
+function cartouchesRestantes(){
+    cartouche.innerHTML = "";
+    for (var i = 6; i > essai; i=i-1) {
+        cartouche.innerHTML += "<div></div>";
+    }
+}
+
+cartouchesRestantes();
+
+
 // Écoute l'événement "submit" du formulaire
 form.addEventListener('submit', function(event) {
         event.preventDefault(); // Empêche le rechargement de la page
         const userNb = parseInt(input.value, 10);
-        if (essai < 6) {
+        if (essai < 5) {
             essai++;
-            
+            cartouchesRestantes();
             if (userNb > goodNb) {
                 feedback.textContent = userNb + " est trop grand ! Essaye encore !";
             } else if (userNb < goodNb) {
@@ -25,7 +37,13 @@ form.addEventListener('submit', function(event) {
                 feedback.textContent = "BRAVO !!! En " + essai + " essais, tu as trouvé le nombre : " + goodNb + ".";
             }
         } else {
-            feedback.textContent = "Tu as été trop long, on recommence avec un nouveau nombre.";
+            feedback.textContent = "Tu as été trop long, le bon nombre était " + goodNb + ". On recommence avec un nouveau nombre.";
+
+            // Affiche le dernier essai fini
+            essai++;
+            cartouchesRestantes();
+
+            // Réinitialise le jeu
             essai = 0;
             goodNb = (nbRandom(1, 100));
         }      
